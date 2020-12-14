f936c38f78c465ab5e7138698d2225faa5d1bcdf
Isaac Muñoz <isaac.munoz-busto-external@capgemini.com>
2020-12-14 13:20:42 +0100

    menu y godMode

---
+++ juego3D/Assets/Scripts/GodMode.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;  
+using System.Collections.Generic;  
+using UnityEngine;  
+using UnityEngine.SceneManagement;
+
+public class GodMode: MonoBehaviour {  
+    void Update()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+
+        if (Input.GetKeyDown(KeyCode.Alpha1) && sceneName != "Level1Scene") SceneManager.LoadScene("Level1Scene");
+        else if (Input.GetKeyDown(KeyCode.Alpha2) && sceneName != "Level2Scene") SceneManager.LoadScene("Level2Scene");
+        else if (Input.GetKeyDown(KeyCode.Alpha3) && sceneName != "Level3Scene") SceneManager.LoadScene("Level3Scene");
+        else if (Input.GetKeyDown(KeyCode.Alpha4) && sceneName != "Level4Scene") SceneManager.LoadScene("Level4Scene");
+        else if (Input.GetKeyDown(KeyCode.Alpha5) && sceneName != "Level5Scene") SceneManager.LoadScene("Level5Scene");
+    }
+}   
---
+++ juego3D/Assets/Scripts/Menu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject mainMenuObject;
+    public GameObject controlsObject;
+    public GameObject creditsObject;
+    public AudioSource selection;
+
+    // Start is called before the first frame update
+    public void StartGame() {
+        selection.Play();
+        SceneManager.LoadScene("Level1Scene", LoadSceneMode.Single);
+    }
+
+    public void EnterControls() {
+        selection.Play();
+        mainMenuObject.SetActive(false);
+        controlsObject.SetActive(true);
+    }
+
+    public void LeaveControls() {
+        selection.Play();
+        mainMenuObject.SetActive(true);
+        controlsObject.SetActive(false);
+    }
+
+    public void EnterSettings() {
+        selection.Play();
+        mainMenuObject.SetActive(false);
+        creditsObject.SetActive(true);
+    }
+
+    public void LeaveSettings() {
+        selection.Play();
+        mainMenuObject.SetActive(true);
+        creditsObject.SetActive(false);
+    }
+}
